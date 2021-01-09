031586c2b85d55e463bee340a39d3f72de092373
RITHEESH R <71439671+RITHEESH123@users.noreply.github.com>
2021-01-09 15:50:57 +0530

    Scripts

---
+++ ColourController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Wikitude;
+using UnityEngine;
+
+public class ColourController : MonoBehaviour
+{
+    public void OnImageRecognized(ImageTarget target)
+    {
+        Color paintColor = Color.white;
+        if (target.Name == "red")
+        {
+            paintColor = Color.red;
+        }
+        else if (target.Name == "blue")
+        {
+            paintColor = Color.blue;
+        }
+
+        GameObject[] cars = GameObject.FindGameObjectsWithTag("car");
+        Debug.Log("Car Count " + cars.Length);
+        foreach(GameObject c in cars)
+        {
+            MeshRenderer mr = c.GetComponentInChildren<MeshRenderer>();
+            Material[] mats = mr.sharedMaterials;
+            foreach(Material m in mats)
+            {
+                m.color = paintColor;
+            }
+        }
+
+    }
+
+    public void OnImageLost(ImageTarget target)
+    {
+        GameObject[] cars = GameObject.FindGameObjectsWithTag("car");
+        Debug.Log("Car Count " + cars.Length);
+        foreach(GameObject c in cars)
+        {
+            MeshRenderer mr = c.GetComponentInChildren<MeshRenderer>();
+            Material[] mats = mr.sharedMaterials;
+            foreach(Material m in mats)
+            {
+                m.color = Color.white;
+            }
+        }
+    }
+}
